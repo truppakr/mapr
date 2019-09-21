ed855f04ea32dd8ad11cdb908591147677b8aebe
truppakr <47297314+truppakr@users.noreply.github.com>
2019-09-21 04:16:24 -0400

    Local
    A sample mapper code to read a tab-delimited line with more than 6 fields and output (print) 3rd and 5th field for a reducer code to process

---
+++ mapper.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 21 03:32:36 2019
+
+@author: rkrishnan
+"""
+
+# Your task is to make sure that this mapper code does not fail on corrupt data lines,
+# but instead just ignores them and continues working
+import sys
+
+def mapper():
+    # read standard input line by line
+    for line in sys.stdin:
+        # strip off extra whitespace, split on tab and put the data in an array
+        
+        if line.count('\t')>=5:
+            data = line.strip().split("\t")
+
+        # This is the place you need to do some defensive programming
+        # what if there are not exactly 6 fields in that line?
+        # YOUR CODE HERE
+        
+        # this next line is called 'multiple assignment' in Python
+        # this is not really necessary, we could access the data
+        # with data[2] and data[5], but we do this for conveniency
+        # and to make the code easier to read
+            date, time, store, item, cost, payment = data
+        
+        # Now print out the data that will be passed to the reducer
+            if len(store)>0 and len(cost)>0 :
+                print("{0}\t{1}".format((store), (cost)))
+        
+test_text = """2013-10-09\t13:22\tMiami\tBoots\t99.95\tVisa
+2013-10-09\t13:22\tNew York\tDVD\t9.50\tMasterCard
+2013-10-09 13:22:59 I/O Error
+^d8x28orz28zoijzu1z1zp1OHH3du3ixwcz114<f
+1\t2\t3"""
+
+# This function allows you to test the mapper with the provided test string
+def main():
+	from io import StringIO
+	sys.stdin = StringIO(test_text)
+	mapper()
+	sys.stdin = sys.__stdin__
